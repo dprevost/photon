3179f54a96afbb57f66f7807bdaacda54a8fb8a2
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2006-11-11 15:59:29 +0000

    Fix the way the exception was handled (some test progs can throw an oserror python exception). Also reorganize the loops to make the code easier to manage/modify.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@28 53c26a9f-4021-0410-9f73-c4284c400f72

--- src/Common/Tests/RunTests.py
+++ src/Common/Tests/RunTests.py
@@ -1,11 +1,37 @@
 import os
 import sys
 
-# List of failed tests
+# List of failed tests. We append to this list when an error is encountered
+# while running the tests
 failed_tests = []
 
-# Programs that should return 0
+# Lists containing the names of the test (one list for each subdir)
+# The "ok" lists are for programs which are expected to return zero (succeed)
+# and the "fail" lists are for the other ones.
 dir_ok_programs = []
+dir_fail_programs = []
+err_ok_programs = []
+err_fail_programs = []
+mem_ok_programs = []
+mem_fail_programs = []
+opt_ok_programs = []
+opt_fail_programs = []
+
+# Lists containing all the info needed to run the for loops at the end of
+# this file. It contains the 2 lists, the prefix used to name the .exe and
+# the exact name of the subdir.
+dir_list = [dir_ok_programs, dir_fail_programs, 'Dir', 'DirAccess']
+err_list = [err_ok_programs, err_fail_programs, 'Err', 'Error']
+mem_list = [mem_ok_programs, mem_fail_programs, 'Mem', 'MemFile']
+opt_list = [opt_ok_programs, opt_fail_programs, 'Opt', 'Options']
+
+# And the master list, the list of all subdir lists.
+master_list = [dir_list, err_list, mem_list, opt_list]
+
+##
+## Now populate the program lists...
+##
+# Programs that should return 0
 dir_ok_programs.append('ClosePass')
 dir_ok_programs.append('FiniPass')
 dir_ok_programs.append('GetNextNoSuchDir')
@@ -15,7 +41,6 @@ dir_ok_programs.append('OpenNoSuchDir')
 dir_ok_programs.append('OpenPass')
 dir_ok_programs.append('Tests')
 
-dir_fail_programs = []
 dir_fail_programs.append('CloseInvalidSig')
 dir_fail_programs.append('CloseNullDir')
 dir_fail_programs.append('FiniInvalidSig')
@@ -33,14 +58,12 @@ dir_fail_programs.append('OpenNullDir')
 dir_fail_programs.append('OpenNullDirname')
 dir_fail_programs.append('OpenNullError')
 
-err_ok_programs = []
 err_ok_programs.append('AddHandlerPass')
 err_ok_programs.append('ChainErrorPass')
 err_ok_programs.append('FiniPass')
 err_ok_programs.append('InitPass')
 err_ok_programs.append('Tests')
 
-err_fail_programs = []
 err_fail_programs.append('AddHandlerNullDef')
 err_fail_programs.append('AddHandlerNullHandler')
 err_fail_programs.append('AddHandlerNullName')
@@ -61,7 +84,6 @@ err_fail_programs.append('SetErrorInvalidSig')
 err_fail_programs.append('SetErrorInvalidValue')
 err_fail_programs.append('SetErrorNullError')
 
-mem_ok_programs = []
 mem_ok_programs.append('BackStatusPass')
 mem_ok_programs.append('ClosePass')
 mem_ok_programs.append('CreateBackPass')
@@ -73,7 +95,6 @@ mem_ok_programs.append('ReadWritePass')
 mem_ok_programs.append('SynchPass')
 mem_ok_programs.append('Tests')
 
-mem_fail_programs = []
 mem_fail_programs.append('BackStatusInvalidSig')
 mem_fail_programs.append('BackStatusMemNull')
 mem_fail_programs.append('BackStatusNameEmpty')
@@ -116,7 +137,6 @@ mem_fail_programs.append('SynchInvalidBaseAddr')
 mem_fail_programs.append('SynchInvalidSig')
 mem_fail_programs.append('SynchMemNull')
 
-opt_ok_programs = []
 opt_ok_programs.append('GetLongOptPass')
 opt_ok_programs.append('GetShortOptPass')
 opt_ok_programs.append('IsLongPresentPass')
@@ -126,7 +146,6 @@ opt_ok_programs.append('ShowUsagePass')
 opt_ok_programs.append('Tests')
 opt_ok_programs.append('ValidatePass')
 
-opt_fail_programs = []
 opt_fail_programs.append('GetLongOptNullHandle')
 opt_fail_programs.append('GetLongOptNullOpt')
 opt_fail_programs.append('GetLongOptNullValue')
@@ -152,135 +171,37 @@ opt_fail_programs.append('ValidateNullArgv')
 opt_fail_programs.append('ValidateNULLArgvi')
 opt_fail_programs.append('ValidateNullHandle')
 
-# DirAccess
-prefix = os.path.join('DirAccess','Release')
-rc = 0
+l = 0
+for my_list in master_list:
+   rc = 0
+   prog_path = os.path.join(my_list[3],'Release')
 
-for program in dir_ok_programs:
-   arg0 = 'Dir' + program
-   exe = 'Dir' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc != 0:
-         raise os.error
-   except:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      raise os.error
+   for program in my_list[0]:
+      exe_name = my_list[2] + program + '.exe'
+      full_name = os.path.join( prog_path, exe_name )
+      try:
+         rc = os.spawnl( os.P_WAIT, full_name, full_name )
+         print 'rc = ', rc
+         if rc != 0:
+            failed_tests.append(os.path.join( full_name, program))
+      except:
+         failed_tests.append(os.path.join( full_name, program))
+         print program, ' failed (spawnl threw an exception)!'
 
-for program in dir_fail_programs:
-   arg0 = 'Dir' + program
-   exe = 'Dir' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc == 0:
-         raise os.error
-   except:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      raise os.error
-
-### Error tests
-
-prefix = os.path.join('Error','Release')
-for program in err_ok_programs:
-   arg0 = 'Err' + program
-   exe = 'Err' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc != 0:
-         raise os.error
-   except:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      raise os.error
-
-for program in err_fail_programs:
-   arg0 = 'Err' + program
-   exe = 'Err' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc == 0:
-         raise os.error
-   except:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      raise os.error
-
-### Memfile tests
-
-prefix = os.path.join('MemFile','Release')
-for program in mem_ok_programs:
-   arg0 = 'Mem' + program
-   exe = 'Mem' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc != 0:
-         raise os.error
-   except:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      raise os.error
-
-for program in mem_fail_programs:
-   arg0 = 'Mem' + program
-   exe = 'Mem' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc == 0:
-         raise os.error
-   except Exception, e:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      print "Exception is:", sys.exc_info()[0], e.args
-#      raise os.error
-
-prefix = os.path.join('Options','Release')
-for program in opt_ok_programs:
-   arg0 = 'Opt' + program
-   exe = 'Opt' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc != 0:
-         raise os.error
-   except:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      raise os.error
-
-for program in opt_fail_programs:
-   arg0 = 'Opt' + program
-   exe = 'Opt' + program + '.exe'
-   path = os.path.join( prefix, exe )
-   try:
-      rc = os.spawnl( os.P_WAIT, path, path )
-      print 'rc = ', rc
-      if rc == 0:
-         raise os.error
-   except Exception, e:
-      failed_tests.append(os.path.join( prefix, program))
-      print program, ' failed! Error = ', rc
-#      print "Exception is:", sys.exc_info()[0], e.args
-#      raise os.error
-
-l  = len(dir_ok_programs)+ len(dir_fail_programs)
-l += len(err_ok_programs)+ len(err_fail_programs)
-l += len(mem_ok_programs)+ len(mem_fail_programs)
-l += len(opt_ok_programs)+ len(opt_fail_programs)
+   for program in my_list[1]:
+      exe_name = my_list[2] + program + '.exe'
+      full_name = os.path.join( prog_path, exe_name )
+      try:
+         rc = os.spawnl( os.P_WAIT, full_name, full_name )
+         print 'rc = ', rc
+         if rc == 0:
+            failed_tests.append(os.path.join( full_name, program))
+      except:
+         failed_tests.append(os.path.join( full_name, program))
+         print program, ' failed (spawnl threw an exception)!'
+   print 'ZZZ ', len(my_list[0]), len(my_list[1])
+   l += len(my_list[0])+ len(my_list[1])
+   
 m = len(failed_tests)
 
 print ''
