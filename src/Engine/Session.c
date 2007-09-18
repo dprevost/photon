831c7ace30a4fa952ef689e07060bc5e080d1fb1
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2007-09-18 19:25:07 +0000

    The old "CleanupSession.cpp" stuff renamed and translated from c++ to c.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@159 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/Engine/Session.c
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2007 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of the vdsf (Virtual Data Space Framework) Library.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Engine/Session.h"
+#include "Engine/MemoryAllocator.h"
+#include "Engine/SessionContext.h"
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int vdseSessionInit( vdseSession        * pSession,
+                     void               * pApiSession,
+                     vdseSessionContext * pContext )
+{
+   vdsErrors errcode;
+   
+   VDS_PRE_CONDITION( pSession  != NULL );
+   VDS_PRE_CONDITION( pContext  != NULL );
+   
+   errcode = vdseMemObjectInit( &pSession->memObject, 
+                                VDSE_IDENT_CLEAN_SESSION,
+                                &pSession->blockGroup,
+                                1 ); /* A single block */
+   if ( errcode != VDS_OK )
+   {
+      vdscSetError( &pContext->errorHandler,
+                    g_vdsErrorHandle,
+                    errcode );
+      return -1;
+   }
+   pContext->pCurrentMemObject = &pSession->memObject;
+
+   pSession->pApiSession = pApiSession;
+   
+   vdseLinkedListInit( &pSession->listOfObjects );
+
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+void vdseSessionFini( vdseSession        * pSession,
+                      vdseSessionContext * pContext )
+{
+   vdseObjectContext* pObject = NULL;
+   vdseLinkNode * pNode = NULL;
+
+   VDS_PRE_CONDITION( pSession != NULL );
+   VDS_PRE_CONDITION( pContext != NULL );
+   VDS_PRE_CONDITION( pSession->memObject.objType == VDSE_IDENT_CLEAN_SESSION );
+   
+   pContext->pCurrentMemObject = &pSession->memObject;
+
+   /*
+    * Eliminate all objects in the list. This is probably not needed
+    * as we will release the blocks of memory to the allocator as the
+    * last step. This might be reviewed eventually.
+    */
+
+   while ( vdseLinkedListPeakFirst( &pSession->listOfObjects, 
+                                    &pNode ) == LIST_OK )
+   {
+      pObject = (vdseObjectContext*)
+         ((char*)pNode - offsetof( vdseObjectContext, node ));
+      vdseSessionRemoveObj( pSession, pObject, pContext );
+   }
+   
+   /* This will remove the blocks of allocated memory */
+   vdseMemObjectFini(  &pSession->memObject, pContext );
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int vdseSessionAddObj( vdseSession        * pSession,
+                       ptrdiff_t            objOffset, 
+                       enum vdsObjectType   objType, 
+                       void *               pProxyObject,
+                       vdseObjectContext ** ppObject,
+                       vdseSessionContext * pContext )
+{
+   int errcode = 0;
+   vdseObjectContext* pCurrentBuffer;
+
+   /* For recovery purposes, always lock before doing anything! */
+   errcode = vdseLock( &pSession->memObject, pContext );
+   if ( errcode == 0 )
+   {
+      pCurrentBuffer = (vdseObjectContext *)
+         vdseMalloc( &pSession->memObject, sizeof(vdseObjectContext), pContext );
+      if ( pCurrentBuffer != NULL )
+      {
+         pCurrentBuffer->offset    = objOffset;
+         pCurrentBuffer->type      = objType;
+         pCurrentBuffer->pProxyObject = pProxyObject;
+
+         vdseLinkedListPutLast( &pSession->listOfObjects, 
+                                &pCurrentBuffer->node );
+         *ppObject = pCurrentBuffer;
+      }
+      else
+         errcode = -1;
+      
+      vdseUnlock( &pSession->memObject, pContext );
+   }
+   else
+      vdscSetError( &pContext->errorHandler,
+                    g_vdsErrorHandle,
+                    VDS_ENGINE_BUSY );
+
+   return errcode;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int vdseSessionRemoveObj( vdseSession        * pSession,
+                          vdseObjectContext  * pObject,
+                          vdseSessionContext * pContext )
+{
+   int errcode = VDS_OK;
+
+   /* For recovery purposes, always lock before doing anything! */
+   errcode = vdseLock( &pSession->memObject, pContext );
+   if ( errcode == 0 )
+   {
+      vdseLinkedListRemoveItem( &pSession->listOfObjects, 
+                                &pObject->node );
+      vdseFree( &pSession->memObject, 
+                (unsigned char *)pObject, 
+                sizeof(vdseObjectContext),
+                pContext );
+
+      vdseUnlock( &pSession->memObject, pContext );
+   }
+   else
+   {
+      vdscSetError( &pContext->errorHandler,
+                    g_vdsErrorHandle,
+                    VDS_ENGINE_BUSY );
+   }
+
+   return errcode;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/* Lock and Unlock must be used before calling this function */
+int vdseSessionRemoveFirst( vdseSession        * pSession,
+                            vdseObjectContext ** ppObject,
+                            vdseSessionContext * pContext )
+{
+   vdseLinkNode * pNode = NULL;
+   int rc;
+   
+   rc = vdseLinkedListGetFirst( &pSession->listOfObjects, 
+                                &pNode );
+   if ( rc == 0 )
+      *ppObject = (vdseObjectContext*)
+         ((char*)pNode - offsetof( vdseObjectContext, node ));
+   
+   return rc;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
---
+++ src/Engine/Session.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2007 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of the vdsf (Virtual Data Space Framework) Library.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#ifndef VDSE_SESSION_H
+#define VDSE_SESSION_H
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Engine/Engine.h"
+#include "Engine/LinkedList.h"
+#include "Engine/LinkNode.h"
+#include "Engine/MemoryObject.h"
+#include "Engine/BlockGroup.h"
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * The context information for each object accessed by a session is 
+ * kept in a doubly linked list (for fast access). 
+ */
+typedef struct vdseObjectContext
+{
+   /** offset to the object pointer in memory */
+   ptrdiff_t          offset;
+
+   /** object type */
+   enum vdsObjectType type;
+
+   /**
+    * Pointer to the object as allocated in the heap of the process.
+    * We cannot use the real type since the Engine does not know about
+    * the API objects.
+    */
+   void * pProxyObject; 
+   
+   /** Our node in the linked list of the vdseSession object. */
+   vdseLinkNode node;
+   
+} vdseObjectContext;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * This object is part of a group of objects which are used for handling 
+ * crashes (process crashes, mainly). They keep track of the current status
+ * and operations done by each process and by doing so enables the watchdog
+ * to cleanup if a process die unexpectedly: undo the current transaction(s),
+ * closed objects (->decrease access counter), unlocked objects, etc.
+ *
+ */
+typedef struct vdseSession
+{
+   /** Always first */
+   struct vdseMemObject memObject;
+
+   /** This object is part of the linked list of the vdseCleanupProcess */
+   vdseLinkNode node;
+
+   /** Pointer to the vdsaSession. To be use by the process object when
+       it cleans up its sessions. */
+   void * pApiSession;
+   
+   /** Our own internal list of objects accessed by the current session */
+   vdseLinkedList listOfObjects;
+
+   /** Variable size struct - always put at the end */
+   struct vdseBlockGroup blockGroup;
+
+} vdseSession;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+VDSF_ENGINE_EXPORT
+int vdseSessionInit( vdseSession        * pSession,
+                     void               * pApiSession,
+                     vdseSessionContext * pContext );
+
+VDSF_ENGINE_EXPORT
+void vdseSessionFini( vdseSession        * pSession,
+                      vdseSessionContext * pContext );
+
+VDSF_ENGINE_EXPORT
+int vdseSessionAddObj( vdseSession        * pSession,
+                       ptrdiff_t            objOffset, 
+                       enum vdsObjectType   objType, 
+                       void *               pProxyObject,
+                       vdseObjectContext ** ppObject,
+                       vdseSessionContext * pContext );
+
+VDSF_ENGINE_EXPORT
+int vdseSessionRemoveObj( vdseSession        * pSession,
+                          vdseObjectContext  * pObject,
+                          vdseSessionContext * pContext );
+
+/* Lock and Unlock must be used before calling this function */
+VDSF_ENGINE_EXPORT
+int vdseSessionRemoveFirst( vdseSession        * pSession,
+                            vdseObjectContext ** ppObject,
+                            vdseSessionContext * pContext );
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#endif /* VDSE_SESSION_H */
+
