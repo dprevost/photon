d7f8ad113207ef6d3f1f7ac6a20615c9c715ae96
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2007-09-21 14:04:05 +0000

    Needed by the watchdog
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@167 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/API/WatchdogCommon.h
@@ -0,0 +1,73 @@
+/* -*- c++ -*- */
+/*
+ * Copyright (C) 2006-2007 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of the vdsf (Virtual Data Space Framework) Library.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#ifndef VDSA_WD_COMMON_H
+#define VDSA_WD_COMMON_H
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/Common.h"
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#define VDS_MEMFILE_NAME  "mem_file"
+#define VDS_LOGDIR_NAME   "log_dir"
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+BEGIN_C_DECLS
+
+/**
+ *  Applications can send two types of messages. One when they first 
+ *  connect to request all needed parameters (file name of VDs, for example)
+ *  and one when they disconnect.
+ */
+enum  WDOpcode
+{
+   WD_CONNECT = 1,
+   WD_DISCONNECT
+};
+
+/**
+ *  This struct is used by the applications to send information to the 
+ *  watchdog.
+ */
+
+struct WDInput
+{
+   enum  WDOpcode opcode;
+   pid_t processId;
+};
+
+
+/**
+ * This struct is used by the watchdog to send information back to the
+ *  applications.
+ */
+
+struct WDOutput
+{
+   char pathname[PATH_MAX];
+   size_t memorySizekb;
+};
+
+END_C_DECLS
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#endif /* VDSA_WD_COMMON_H */
