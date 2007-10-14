bc37269c7e54d3d768d4e5997cc3d89ffffd8937
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2007-10-14 04:56:43 +0000

    Unit tests for api/hashmap.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@232 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/API/Tests/HashMap/GetPass.c
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2007 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/Common.h"
+#include <vdsf/vds.h>
+#include "Tests/PrintError.h"
+#include "API/HashMap.h"
+
+const bool expectedToPass = true;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main( int argc, char * argv[] )
+{
+   VDS_HANDLE handle, sessionHandle;
+   int errcode;
+   vdsaCommonObject object;
+   const char * key  = "My Key";
+   const char * data = "My Data";
+   vdsHashMapEntry entry;
+
+   if ( argc > 1 )
+      errcode = vdsInit( argv[1], 0, &handle );
+   else
+      errcode = vdsInit( "10701", 0, &handle );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdsInitSession( &sessionHandle );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsCreateObject( sessionHandle,
+                              "/ahgp",
+                              VDS_FOLDER );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsCreateObject( sessionHandle,
+                              "/ahgp/test",
+                              VDS_HASH_MAP );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsHashMapOpen( sessionHandle,
+                            "/ahgp/test",
+                            &handle );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsHashMapInsert( handle,
+                               key,
+                               6,
+                               data,
+                               7 );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsHashMapGet( handle,
+                            key,
+                            6,
+                            &entry );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   if ( memcmp( entry.data, data, 7 ) != 0 )
+      ERROR_EXIT( expectedToPass, NULL, ; );
+
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
