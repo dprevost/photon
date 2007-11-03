601cb41ef0f858173e5374cbac2575a497d4b932
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2007-11-03 00:31:14 +0000

    Adding tests to Api/Queue.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@278 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/API/Tests/Queue/Pop.c
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2007 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/Common.h"
+#include <vdsf/vds.h>
+#include "Tests/PrintError.h"
+#include "API/Queue.h"
+
+const bool expectedToPass = true;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main( int argc, char * argv[] )
+{
+   VDS_HANDLE handle, sessionHandle;
+   int errcode;
+   const char * data1 = "My Data1";
+   char buffer[200];
+   size_t length;
+
+   if ( argc > 1 )
+      errcode = vdsInit( argv[1], 0, &handle );
+   else
+      errcode = vdsInit( "10701", 0, &handle );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdsInitSession( &sessionHandle );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsCreateObject( sessionHandle,
+                              "/aqpopp",
+                              strlen("/aqpopp"),
+                              VDS_FOLDER );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsCreateObject( sessionHandle,
+                              "/aqpopp/test",
+                              strlen("/aqpopp/test"),
+                              VDS_QUEUE );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsQueueOpen( sessionHandle,
+                           "/aqpopp/test",
+                           strlen("/aqpopp/test"),
+                           &handle );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsQueuePush( handle, data1, strlen(data1) );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = vdsQueuePop( NULL,
+                          buffer,
+                          200,
+                          &length );
+   if ( errcode != VDS_NULL_HANDLE )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdsQueuePop( handle,
+                          NULL,
+                          200,
+                          &length );
+   if ( errcode != VDS_NULL_POINTER )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdsQueuePop( handle,
+                          buffer,
+                          2,
+                          &length );
+   if ( errcode != VDS_INVALID_LENGTH )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdsQueuePop( handle,
+                          buffer,
+                          200,
+                          NULL );
+   if ( errcode != VDS_NULL_POINTER )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdsQueuePop( handle,
+                          buffer,
+                          200,
+                          &length );
+   if ( errcode != VDS_OK )
+   {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   if ( memcmp( buffer, data1, strlen(data1) ) != 0 )
+      ERROR_EXIT( expectedToPass, NULL, ; );
+
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
