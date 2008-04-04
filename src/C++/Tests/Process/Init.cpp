a5d90b02c1a55309b8706c03238ee8a7fbdf924f
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2008-04-04 03:36:05 +0000

    Adding test for c++ vdsProcess.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@537 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/C++/Tests/Process/Init.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/Common.h"
+#include <vdsf/vds>
+#include <iostream>
+
+using namespace std;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main( int argc, char * argv[] )
+{
+  vdsProcess process;
+   
+   try {
+      process.Init( NULL );
+      // Should never come here
+      cerr << "Test failed - line " << __LINE__ << endl;
+      return 1;
+   }
+   catch( vdsException exc ) {
+      if ( exc.ErrorCode() != VDS_INVALID_WATCHDOG_ADDRESS ) {
+         cerr << "Test failed - line " << __LINE__ << ", error = " << exc.Message() << endl;
+         return 1;
+      }
+   }
+
+   try {
+      process.Init( "12345", true );
+      // Should never come here
+      cerr << "Test failed - line " << __LINE__ << endl;
+      return 1;
+   }
+   catch( vdsException exc ) {
+      if ( exc.ErrorCode() != VDS_CONNECT_ERROR ) {
+         cerr << "Test failed - line " << __LINE__ << ", error = " << exc.Message() << endl;
+         return 1;
+      }
+   }
+
+   try {
+      process.Init( "12345", false );
+      // Should never come here
+      cerr << "Test failed - line " << __LINE__ << endl;
+      return 1;
+   }
+   catch( vdsException exc ) {
+      if ( exc.ErrorCode() != VDS_CONNECT_ERROR ) {
+         cerr << "Test failed - line " << __LINE__ << ", error = " << exc.Message() << endl;
+         return 1;
+      }
+   }
+
+   try {
+      if ( argc > 1 ) {
+         process.Init( argv[1] );
+      }
+      else {
+         process.Init( "10701" );
+      }
+   }
+   catch( vdsException exc ) {
+      cerr << "Test failed - line " << __LINE__ << ", error = " << exc.Message() << endl;
+      cerr << "Is the watchdog running?" << endl;
+      return 1;
+   }
+
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
