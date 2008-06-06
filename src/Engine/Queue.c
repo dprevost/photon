bc318d62953fd6d6dd60a710200726834f74a579
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2008-06-06 13:50:00 +0000

    Adding new tests.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@586 53c26a9f-4021-0410-9f73-c4284c400f72

--- src/Engine/Queue.c
+++ src/Engine/Queue.c
@@ -248,7 +248,6 @@ int vdseQueueInit( vdseQueue           * pQueue,
                    char                * origName,
                    ptrdiff_t             keyOffset,
                    vdsObjectDefinition * pDefinition,
-//                   int                  numFields,
                    vdseSessionContext  * pContext )
 {
    vdsErrors errcode;
@@ -261,7 +260,7 @@ int vdseQueueInit( vdseQueue           * pQueue,
    VDS_PRE_CONDITION( parentOffset != NULL_OFFSET );
    VDS_PRE_CONDITION( numberOfBlocks > 0 );
    VDS_PRE_CONDITION( origNameLength > 0 );
-//   VDS_PRE_CONDITION( numFields      > 0 );
+   VDS_PRE_CONDITION( pDefinition->numFields > 0 );
    
    errcode = vdseMemObjectInit( &pQueue->memObject, 
                                 VDSE_IDENT_QUEUE,
