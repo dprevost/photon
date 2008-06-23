d5bda5718ff82a92980d4827a710012f7c6c542b
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2008-06-23 15:12:18 +0000

    Added support for Intel compiler on i386 and x86_64 since it supports gcc inline asm on these platforms (itanium is different however). Also added barriers on itanium for gcc and icc. Locking code is not done yet so Itanium is still unsupported.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@634 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/Common/arch/Barrier-ia64-icc.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2007-2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of the vdsf (Virtual Data Space Framework) Library.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#ifndef VDSC_ARCH_BARRIER_H
+#define VDSC_ARCH_BARRIER_H
+
+/*
+ * Barrier for the Itanium with the Intel compiler (intrinsics).
+ */
+
+#include <ia64intrin.h>
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+static inline void vdscMemoryBarrier()
+{
+   __mf();
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+static inline void vdscReadMemoryBarrier()
+{
+   __mf();
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+static inline void vdscWriteMemoryBarrier()
+{
+   __mf();
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#endif /* VDSC_ARCH_BARRIER_H */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
