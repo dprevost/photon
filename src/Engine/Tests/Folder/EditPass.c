ba1f45dbe7cf48111aa580c4a82b55efb4abca7f
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2008-06-27 15:36:20 +0000

    Adding tests to Engine/Folder.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@651 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/Engine/Tests/Folder/EditPass.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2007-2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "folderTest.h"
+
+const bool expectedToPass = true;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main()
+{
+   vdseFolder* pFolder;
+   vdseSessionContext context;
+   int errcode;
+   vdseTxStatus status;
+   vdseFolderItem folderItem;
+   vdseObjectDescriptor * pDescriptor;
+   vdseTxStatus * txItemStatus;
+   vdseTreeNode * pNode;
+   vdsObjectDefinition def = { 
+      VDS_FOLDER, 
+      0, 
+      { 0, 0, 0, 0}, 
+      { { "", 0, 0, 0, 0, 0, 0} } 
+   };
+
+   pFolder = initFolderTest( expectedToPass, &context );
+
+   vdseTxStatusInit( &status, SET_OFFSET( context.pTransaction ) );
+   
+   errcode = vdseFolderInit( pFolder, 0, 1, 0, &status, 5, "Test1", 1234, &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   
+   errcode = vdseFolderInsertObject( pFolder,
+                                     "test2",
+                                     "Test2",
+                                     5,
+                                     &def,
+                                     1,
+                                     0,
+                                     &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   
+   errcode = vdseFolderEditObject( pFolder,
+                                  "test2",
+                                  5,
+                                  VDS_FOLDER,
+                                  &folderItem,
+                                  &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   GET_PTR( pDescriptor, folderItem.pHashItem->dataOffset, vdseObjectDescriptor );
+   if ( memcmp( pDescriptor->originalName, 
+                "Test2", 5*sizeof(char) ) != 0 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   GET_PTR( pNode, pDescriptor->nodeOffset, vdseTreeNode);
+   GET_PTR( txItemStatus, pNode->txStatusOffset, vdseTxStatus );
+   if ( txItemStatus->parentCounter != 1 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   if ( status.usageCounter != 1 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = vdseFolderEditObject( pFolder,
+                                  "test3",
+                                  5,
+                                  VDS_FOLDER,
+                                  &folderItem,
+                                  &context );
+   if ( errcode != -1 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   if ( vdscGetLastError( &context.errorHandler ) != VDS_NO_SUCH_OBJECT ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   
+   errcode = vdseFolderInsertObject( pFolder,
+                                     "test4",
+                                     "Test4",
+                                     5,
+                                     &def,
+                                     1,
+                                     0,
+                                     &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   
+   errcode = vdseFolderEditObject( pFolder,
+                                  "test4",
+                                  5,
+                                  VDS_FOLDER,
+                                  &folderItem,
+                                  &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   GET_PTR( pDescriptor, folderItem.pHashItem->dataOffset, vdseObjectDescriptor );
+   if ( memcmp( pDescriptor->originalName, 
+                "Test4", 5*sizeof(char) ) != 0 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   GET_PTR( pNode, pDescriptor->nodeOffset, vdseTreeNode);
+   GET_PTR( txItemStatus, pNode->txStatusOffset, vdseTxStatus );
+   if ( txItemStatus->parentCounter != 1 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   if ( status.usageCounter != 2 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   if ( pFolder->nodeObject.txCounter != 2 ) {
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   vdseFolderFini( pFolder, &context );
+   
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
---
+++ src/Engine/Tests/Map/hashMapTest.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2007-2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#ifndef VDST_HASH_MAP_TEST_H
+#define VDST_HASH_MAP_TEST_H
+
+#include "Engine/Engine.h"
+#include "Engine/MemoryObject.h"
+#include "Engine/BlockGroup.h"
+#include "Engine/MemoryAllocator.h"
+#include "Engine/Transaction.h"
+#include "Engine/InitEngine.h"
+#include "Engine/Folder.h"
+#include "Engine/Map.h"
+#include "Tests/PrintError.h"
+
+VDSF_ENGINE_EXPORT
+vdscErrMsgHandle g_vdsErrorHandle;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * This function initializes the memory needed by the hash map
+ * object.
+ *
+ * A reminder: hash maps are memory objects - when they need memory, 
+ * they ask it from their internal memObject which can then ask the 
+ * global allocator. This requires proper initialization.
+ *
+ * The returned object is not initialized however - so that we can test
+ * the Init() call.
+ */
+ 
+vdseMap * 
+initHashMapTest( bool                testIsExpectedToSucceed,
+                 vdseSessionContext* pContext )
+{
+   int errcode;
+   unsigned char* ptr;
+   vdseMemAlloc*  pAlloc;
+   vdseTx* pTx;
+   vdseMap* pHashMap;
+   size_t allocatedLength = VDSE_BLOCK_SIZE * 25;
+
+   memset( pContext, 0, sizeof(vdseSessionContext) );
+   pContext->pidLocker = getpid();
+   
+   errcode = vdseInitEngine();
+   if ( errcode != 0 ) {
+      fprintf( stderr, "Abnormal error at line %d in folderTest.h\n", __LINE__ );
+      if ( testIsExpectedToSucceed ) exit(1);
+      exit(0);
+   }
+   vdscInitErrorHandler( &pContext->errorHandler );
+
+   /* Initialize the global allocator */
+   ptr = malloc( allocatedLength );
+   if (ptr == NULL ) {
+      fprintf( stderr, "Abnormal error at line %d in folderTest.h\n", __LINE__ );
+      if ( testIsExpectedToSucceed ) exit(1);
+      exit(0);
+   }
+   g_pBaseAddr = ptr;
+   pAlloc = (vdseMemAlloc*)(g_pBaseAddr + VDSE_BLOCK_SIZE);
+   vdseMemAllocInit( pAlloc, ptr, allocatedLength, pContext );
+   
+   /* Allocate memory for the tx object and initialize it */
+   pTx = (vdseTx*)vdseMallocBlocks( pAlloc, VDSE_ALLOC_ANY, 1, pContext );
+   if ( pTx == NULL ) {
+      fprintf( stderr, "Abnormal error at line %d in folderTest.h\n", __LINE__ );
+      if ( testIsExpectedToSucceed ) exit(1);
+      exit(0);
+   }
+   errcode = vdseTxInit( pTx, 1, pContext );
+   if ( errcode != 0 ) {
+      fprintf( stderr, "Abnormal error at line %d in folderTest.h\n", __LINE__ );
+      if ( testIsExpectedToSucceed ) exit(1);
+      exit(0);
+   }
+   pContext->pTransaction = pTx;
+   
+   /* Allocate memory for the hash map object */
+   pHashMap = (vdseMap*)vdseMallocBlocks( pAlloc, VDSE_ALLOC_API_OBJ, 1, pContext );
+   if ( pHashMap == NULL ) {
+      fprintf( stderr, "Abnormal error at line %d in folderTest.h\n", __LINE__ );
+      if ( testIsExpectedToSucceed ) exit(1);
+      exit(0);
+   }
+   
+   return pHashMap;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#endif /* VDST_HASH_MAP_TEST_H */
+
