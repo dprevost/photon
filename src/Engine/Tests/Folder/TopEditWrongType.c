72a532d5da6af92822e721596fde039cf7951e9d
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2008-06-27 04:34:35 +0000

    Adding tests to engine/folder
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@649 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/Engine/Tests/Folder/TopEditWrongType.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2007-2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "folderTest.h"
+
+const bool expectedToPass = false;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main()
+{
+#if defined(USE_DBC)
+   vdseFolder * pTopFolder;
+   vdseSessionContext context;
+   int errcode;
+   vdseFolderItem folderItem;
+   vdsObjectDefinition def = { 
+      VDS_FOLDER, 
+      0, 
+      { 0, 0, 0, 0}, 
+      { { "", 0, 0, 0, 0, 0, 0} } 
+   };
+   
+   pTopFolder = initTopFolderTest( expectedToPass, &context );
+
+   errcode = vdseTopFolderCreateObject( pTopFolder,
+                                        "Test1",
+                                        strlen("Test1"),
+                                        &def,
+                                        &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   
+   errcode = vdseTopFolderCreateObject( pTopFolder,
+                                        "Test1/Test2",
+                                        strlen("Test1/Test2"),
+                                        &def,
+                                        &context );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &context.errorHandler, ; );
+   }
+   
+   errcode = vdseTopFolderEditObject( pTopFolder,
+                                      "Test1/Test2",
+                                      strlen("Test1/Test2"),
+                                      0,
+                                      &folderItem,
+                                      &context );
+
+   ERROR_EXIT( expectedToPass, NULL, ; );
+#else
+   return 1;
+#endif
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
