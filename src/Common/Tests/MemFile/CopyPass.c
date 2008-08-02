b7edde2520c8d534bdc685ce5ccf44f04dd49ac0
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2008-08-02 17:15:49 +0000

    Added a copy function to the memory file in order to create a backup on startup.
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@730 53c26a9f-4021-0410-9f73-c4284c400f72

---
+++ src/Common/Tests/MemFile/CopyPass.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2008 Daniel Prevost <dprevost@users.sourceforge.net>
+ *
+ * This file is part of vdsf (Virtual Data Space Framework).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file COPYING included in the
+ * packaging of this library.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/MemoryFile.h"
+#include "Tests/PrintError.h"
+
+const bool expectedToPass = true;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main()
+{
+   vdscMemoryFile  mem;
+   vdscErrorHandler errorHandler;
+   void * pAddr = NULL;
+   int errcode = 0;
+   
+   /* The rename is a work around for a bug on Windows. It seems that the delete
+    * call is not as synchroneous as it should be...
+    */
+   rename( "MemFile.mem", "MemFile.old" );
+   unlink( "MemFile.old" );
+   
+   vdscInitErrorDefs();
+   vdscInitErrorHandler( &errorHandler );
+   vdscInitMemoryFile( &mem, 2049, "MemFile.mem" );
+
+   errcode = vdscCreateBackstore( &mem, 0755, &errorHandler );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &errorHandler, unlink( "MemFile.mem" ) );
+   }
+   
+   errcode = vdscCopyBackstore( &mem, 0755, &errorHandler );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &errorHandler, 
+         { unlink( "MemFile.mem" ); unlink( "MemFile.mem.bck" ); } );
+   }
+
+   errcode = vdscCopyBackstore( &mem, 0755, &errorHandler );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &errorHandler, 
+         { unlink( "MemFile.mem" ); unlink( "MemFile.mem.bck" ); } );
+   }
+   
+   unlink( "MemFile.mem" );
+//   unlink( "MemFile.mem" );
+   vdscFiniMemoryFile( &mem );
+
+   vdscInitMemoryFile( &mem, 9, "MemFile.mem" );
+   errcode = vdscCreateBackstore( &mem, 0755, &errorHandler );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &errorHandler, 
+         { unlink( "MemFile.mem" ); unlink( "MemFile.mem.bck" ); } );
+   }
+   
+   errcode = vdscCopyBackstore( &mem, 0755, &errorHandler );
+   if ( errcode != 0 ) {
+      ERROR_EXIT( expectedToPass, &errorHandler, 
+         { unlink( "MemFile.mem" ); unlink( "MemFile.mem.bck" ); } );
+   }
+   
+   unlink( "MemFile.mem" );
+   unlink( "MemFile.mem.bck" );
+   vdscFiniMemoryFile( &mem );
+
+   vdscFiniErrorHandler( &errorHandler );
+   vdscFiniErrorDefs();
+
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
