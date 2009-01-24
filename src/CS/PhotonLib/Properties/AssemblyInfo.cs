9c35a7b388b1f83dd73d6e9661671442509462a5
dprevost <dprevost@53c26a9f-4021-0410-9f73-c4284c400f72>
2009-01-24 21:59:39 +0000

    Making the project files work for vs2005 and vs2008 (work in progress).
    git-svn-id: https://vdsf.svn.sourceforge.net/svnroot/vdsf/trunk@930 53c26a9f-4021-0410-9f73-c4284c400f72

--- src/CS/PhotonLib/Properties/AssemblyInfo.cs
+++ src/CS/PhotonLib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("photonsoftware.org")]
 [assembly: AssemblyProduct("PhotonLib")]
-[assembly: AssemblyCopyright("Copyright © 2008 Daniel Prevost <dprevost@photonsoftware.org>")]
+[assembly: AssemblyCopyright("Copyright © 2008-2009 Daniel Prevost <dprevost@photonsoftware.org>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
