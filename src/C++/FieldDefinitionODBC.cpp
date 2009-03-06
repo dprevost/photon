2eb844c68063900c082e58d58e1f8ab6109de1e1
Daniel Prevost <dprevost@users.sourceforge.net>
2009-03-06 08:08:51 -0500

    Added GetNext() method to Field/KeyDefinition c++ classes + first pass at fixing the man pages.

--- src/C++/FieldDefinitionODBC.cpp
+++ src/C++/FieldDefinitionODBC.cpp
@@ -31,10 +31,17 @@ FieldDefinitionODBC::FieldDefinitionODBC( unsigned char * serialFieldDef,
                                           uint32_t        fieldDefLen )
    : FieldDefinition( serialFieldDef, fieldDefLen ),
      field        ( NULL ),
-     numFields    ( 0 ),
      currentField ( 0 ),
      simpleDef    ( true )
 {
+   numFields = fieldDefLen / sizeof(psoFieldDefinition);
+
+   field = (psoFieldDefinition *) malloc( fieldDefLen );
+   if ( field == NULL ) {
+      throw pso::Exception( "FieldDefinitionODBC::FieldDefinitionODBC",
+                            PSO_NOT_ENOUGH_HEAP_MEMORY );
+   }
+   memcpy( field, serialFieldDef, fieldDefLen );
 }
 
 // --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
@@ -86,104 +93,6 @@ void FieldDefinitionODBC::AddField( std::string  & name,
 }
 
 // --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
-#if 0
-void FieldDefinitionODBC::AddField( const char   * name,
-                                     uint32_t       nameLength,
-                                     psoFieldType   type,
-                                     uint32_t       length,
-                                     uint32_t       precision,
-                                     uint32_t       scale )
-{
-   if ( fields == NULL ) {
-      throw pso::Exception( "FieldDefinitionODBC::AddField", PSO_NULL_POINTER );
-   }
-
-   if ( currentField >= numFields ) {
-      throw pso::Exception( "FieldDefinitionODBC::AddField",
-                            PSO_INVALID_NUM_FIELDS );
-   }
-   
-   if ( nameLength == 0 || nameLength > PSO_MAX_FIELD_LENGTH ) {
-      throw pso::Exception( "FieldDefinitionODBC::AddField",
-                            PSO_INVALID_FIELD_NAME );
-   }
-   memcpy( fields[currentField].name, name, nameLength );
-   
-   switch ( type ) {
-   case PSO_INTEGER:
-   case PSO_TINYINT:
-   case PSO_SMALLINT:
-   case PSO_BIGINT:
-   case PSO_REAL:
-   case PSO_DOUBLE:
-   case PSO_DATE:
-   case PSO_TIME:
-   case PSO_TIMESTAMP:
-      fields[currentField].type = type;
-      fields[currentField].vals.length = 0;
-      currentField++;
-      break;
-
-   case PSO_BINARY:
-   case PSO_CHAR:
-      if ( length == 0 ) {
-         throw pso::Exception( "FieldDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_LENGTH );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.length = length;
-      currentField++;
-      break;
-
-   case PSO_VARBINARY:
-   case PSO_VARCHAR:
-      if ( currentField != numFields-1 ) {
-         throw pso::Exception( "FieldDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_TYPE );
-      }
-      if ( length == 0 ) {
-         throw pso::Exception( "FieldDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_LENGTH );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.length = length;
-      currentField++;
-      break;
-
-   case PSO_LONGVARBINARY:
-   case PSO_LONGVARCHAR:
-      if ( currentField != numFields-1 ) {
-         throw pso::Exception( "FieldDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_TYPE );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.length = 0;
-      currentField++;
-      break;
-
-   case PSO_NUMERIC:
-      if ( precision == 0 || precision > PSO_FIELD_MAX_PRECISION ) {
-         throw pso::Exception( "FieldDefinitionODBC::AddField",
-                               PSO_INVALID_PRECISION );
-      }
-      if ( scale > precision ) {
-         throw pso::Exception( "FieldDefinitionODBC::AddField",
-                               PSO_INVALID_SCALE );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.decimal.precision = precision;
-      fields[currentField].vals.decimal.scale = scale;
-      currentField++;
-      break;
-
-   default:
-      throw pso::Exception( "FieldDefinitionODBC::AddField",
-                            PSO_INVALID_FIELD_TYPE );
-   }
-}
-#endif
-
-// --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
 
 void FieldDefinitionODBC::AddField( const char * name,
                                     uint32_t     nameLength,
@@ -192,6 +101,11 @@ void FieldDefinitionODBC::AddField( const char * name,
                                     uint32_t     precision,
                                     uint32_t     scale )
 {
+   if ( readOnly ) {
+      throw pso::Exception( "FieldDefinitionODBC::AddField",
+                            PSO_INVALID_DEF_OPERATION );
+   }
+
    if ( field == NULL ) {
       throw pso::Exception( "FieldDefinitionODBC::AddField", PSO_NULL_POINTER );
    }
@@ -279,6 +193,107 @@ void FieldDefinitionODBC::AddField( const char * name,
       throw pso::Exception( "FieldDefinitionODBC::AddField",
                             PSO_INVALID_FIELD_TYPE );
    }
+}
+
+// --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
+
+string FieldDefinitionODBC::GetNext() 
+{
+   string s;
+   char name [PSO_MAX_FIELD_LENGTH+1];
+   
+   if ( ! readOnly ) {
+      throw pso::Exception( "FieldDefinitionODBC::GetNext",
+                            PSO_INVALID_DEF_OPERATION );
+   }
+   if ( field == NULL ) {
+      throw pso::Exception( "FieldDefinitionODBC::GetNext", PSO_NULL_POINTER );
+   }
+
+   if ( currentField >= numFields ) return s;
+
+   s += "Name: ";   
+   if ( field[currentField].name[PSO_MAX_FIELD_LENGTH-1] == '\0' ) {
+      s += field[currentField].name;
+   }
+   else {
+      memcpy( name, field[currentField].name, PSO_MAX_FIELD_LENGTH );
+      name[PSO_MAX_FIELD_LENGTH] = '\0';
+      s += name;
+   }
+      
+   s += ", Type: ";
+   switch ( field[currentField].type ) {
+
+   case PSO_TINYINT:
+      s += "TinyInt";
+      break;
+   case PSO_SMALLINT:
+      s += "SmallInt";
+      break;
+   case PSO_INTEGER:
+      s += "Integer";
+      break;
+   case PSO_BIGINT:
+      s += "BigInt";
+      break;
+   case PSO_REAL:
+      s += "Real";
+      break;
+   case PSO_DOUBLE:
+      s += "Double";
+      break;
+   case PSO_DATE:
+      s += "Date";
+      break;
+   case PSO_TIME:
+      s += "Time";
+      break;
+   case PSO_TIMESTAMP:
+      s += "TimeStamp";
+      break;
+
+   case PSO_BINARY:
+      s += "Binary, Length: ";
+      s += field[currentField].vals.length;
+      break;
+   case PSO_CHAR:
+      s += "Char, Length: ";
+      s += field[currentField].vals.length;
+      break;
+
+   case PSO_VARBINARY:
+      s += "VarBinary, Length: ";
+      s += field[currentField].vals.length;
+      break;
+
+   case PSO_VARCHAR:
+      s += "VarChar, Length: ";
+      s += field[currentField].vals.length;
+      break;
+
+   case PSO_LONGVARBINARY:
+      s += "LongVarBinary";
+      break;
+   case PSO_LONGVARCHAR:
+      s += "LongVarChar";
+      break;
+
+   case PSO_NUMERIC:
+      s += "Numeric, Precision = ";
+      s += field[currentField].vals.decimal.precision;
+      s += ", Scale = ";
+      s += field[currentField].vals.decimal.scale;
+      break;
+
+   default:
+      throw pso::Exception( "FieldDefinitionODBC::GetNext",
+                            PSO_INVALID_FIELD_TYPE );
+   }
+
+   currentField++;
+
+   return s;
 }
 
 // --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
--- src/C++/KeyDefinitionODBC.cpp
+++ src/C++/KeyDefinitionODBC.cpp
@@ -82,104 +82,6 @@ void KeyDefinitionODBC::AddKeyField( std::string & name,
 }
 
 // --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
-#if 0
-void KeyDefinitionODBC::AddField( const char   * name,
-                                     uint32_t       nameLength,
-                                     psoFieldType   type,
-                                     uint32_t       length,
-                                     uint32_t       precision,
-                                     uint32_t       scale )
-{
-   if ( fields == NULL ) {
-      throw pso::Exception( "KeyDefinitionODBC::AddField", PSO_NULL_POINTER );
-   }
-
-   if ( currentField >= numFields ) {
-      throw pso::Exception( "KeyDefinitionODBC::AddField",
-                            PSO_INVALID_NUM_FIELDS );
-   }
-   
-   if ( nameLength == 0 || nameLength > PSO_MAX_FIELD_LENGTH ) {
-      throw pso::Exception( "KeyDefinitionODBC::AddField",
-                            PSO_INVALID_FIELD_NAME );
-   }
-   memcpy( fields[currentField].name, name, nameLength );
-   
-   switch ( type ) {
-   case PSO_INTEGER:
-   case PSO_TINYINT:
-   case PSO_SMALLINT:
-   case PSO_BIGINT:
-   case PSO_REAL:
-   case PSO_DOUBLE:
-   case PSO_DATE:
-   case PSO_TIME:
-   case PSO_TIMESTAMP:
-      fields[currentField].type = type;
-      fields[currentField].vals.length = 0;
-      currentField++;
-      break;
-
-   case PSO_BINARY:
-   case PSO_CHAR:
-      if ( length == 0 ) {
-         throw pso::Exception( "KeyDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_LENGTH );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.length = length;
-      currentField++;
-      break;
-
-   case PSO_VARBINARY:
-   case PSO_VARCHAR:
-      if ( currentField != numFields-1 ) {
-         throw pso::Exception( "KeyDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_TYPE );
-      }
-      if ( length == 0 ) {
-         throw pso::Exception( "KeyDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_LENGTH );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.length = length;
-      currentField++;
-      break;
-
-   case PSO_LONGVARBINARY:
-   case PSO_LONGVARCHAR:
-      if ( currentField != numFields-1 ) {
-         throw pso::Exception( "KeyDefinitionODBC::AddField",
-                               PSO_INVALID_FIELD_TYPE );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.length = 0;
-      currentField++;
-      break;
-
-   case PSO_NUMERIC:
-      if ( precision == 0 || precision > PSO_FIELD_MAX_PRECISION ) {
-         throw pso::Exception( "KeyDefinitionODBC::AddField",
-                               PSO_INVALID_PRECISION );
-      }
-      if ( scale > precision ) {
-         throw pso::Exception( "KeyDefinitionODBC::AddField",
-                               PSO_INVALID_SCALE );
-      }
-      fields[currentField].type = type;
-      fields[currentField].vals.decimal.precision = precision;
-      fields[currentField].vals.decimal.scale = scale;
-      currentField++;
-      break;
-
-   default:
-      throw pso::Exception( "KeyDefinitionODBC::AddField",
-                            PSO_INVALID_FIELD_TYPE );
-   }
-}
-#endif
-
-// --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
 
 void KeyDefinitionODBC::AddKeyField( const char * name,
                                      uint32_t     nameLength,
@@ -203,18 +105,18 @@ void KeyDefinitionODBC::AddKeyField( const char * name,
    
    
    switch ( type ) {
-   case PSO_INTEGER:
-   case PSO_BIGINT:
-   case PSO_DATE:
-   case PSO_TIME:
-   case PSO_TIMESTAMP:
+   case PSO_KEY_INTEGER:
+   case PSO_KEY_BIGINT:
+   case PSO_KEY_DATE:
+   case PSO_KEY_TIME:
+   case PSO_KEY_TIMESTAMP:
       key[currentKey].type = type;
       key[currentKey].length = 0;
       currentKey++;
       break;
 
-   case PSO_BINARY:
-   case PSO_CHAR:
+   case PSO_KEY_BINARY:
+   case PSO_KEY_CHAR:
       if ( length == 0 ) {
          throw pso::Exception( "KeyDefinitionODBC::AddKeyField",
                                PSO_INVALID_FIELD_LENGTH );
@@ -224,8 +126,8 @@ void KeyDefinitionODBC::AddKeyField( const char * name,
       currentKey++;
       break;
 
-   case PSO_VARBINARY:
-   case PSO_VARCHAR:
+   case PSO_KEY_VARBINARY:
+   case PSO_KEY_VARCHAR:
       if ( currentKey != numKeys-1 ) {
          throw pso::Exception( "KeyDefinitionODBC::AddKeyField",
                                PSO_INVALID_FIELD_TYPE );
@@ -239,8 +141,8 @@ void KeyDefinitionODBC::AddKeyField( const char * name,
       currentKey++;
       break;
 
-   case PSO_LONGVARBINARY:
-   case PSO_LONGVARCHAR:
+   case PSO_KEY_LONGVARBINARY:
+   case PSO_KEY_LONGVARCHAR:
       if ( currentKey != numKeys-1 ) {
          throw pso::Exception( "KeyDefinitionODBC::AddKeyField",
                                PSO_INVALID_FIELD_TYPE );
@@ -254,6 +156,88 @@ void KeyDefinitionODBC::AddKeyField( const char * name,
       throw pso::Exception( "KeyDefinitionODBC::AddKeyLength",
                             PSO_INVALID_FIELD_TYPE );
    }
+}
+
+// --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
+
+string KeyDefinitionODBC::GetNext() 
+{
+   string s;
+   char name [PSO_MAX_FIELD_LENGTH+1];
+   
+   if ( ! readOnly ) {
+      throw pso::Exception( "KeyDefinitionODBC::GetNext",
+                            PSO_INVALID_DEF_OPERATION );
+   }
+   if ( key == NULL ) {
+      throw pso::Exception( "KeyDefinitionODBC::GetNext", PSO_NULL_POINTER );
+   }
+
+   if ( currentKey >= numKeys ) return s;
+
+   s += "Name: ";   
+   if ( key[currentKey].name[PSO_MAX_FIELD_LENGTH-1] == '\0' ) {
+      s += key[currentKey].name;
+   }
+   else {
+      memcpy( name, key[currentKey].name, PSO_MAX_FIELD_LENGTH );
+      name[PSO_MAX_FIELD_LENGTH] = '\0';
+      s += name;
+   }
+      
+   s += ", Type: ";
+   switch ( key[currentKey].type ) {
+
+   case PSO_KEY_INTEGER:
+      s += "Integer";
+      break;
+   case PSO_KEY_BIGINT:
+      s += "BigInt";
+      break;
+   case PSO_KEY_DATE:
+      s += "Date";
+      break;
+   case PSO_KEY_TIME:
+      s += "Time";
+      break;
+   case PSO_KEY_TIMESTAMP:
+      s += "TimeStamp";
+      break;
+
+   case PSO_KEY_BINARY:
+      s += "Binary, Length: ";
+      s += key[currentKey].length;
+      break;
+   case PSO_KEY_CHAR:
+      s += "Char, Length: ";
+      s += key[currentKey].length;
+      break;
+
+   case PSO_KEY_VARBINARY:
+      s += "VarBinary, Length: ";
+      s += key[currentKey].length;
+      break;
+
+   case PSO_KEY_VARCHAR:
+      s += "VarChar, Length: ";
+      s += key[currentKey].length;
+      break;
+
+   case PSO_KEY_LONGVARBINARY:
+      s += "LongVarBinary";
+      break;
+   case PSO_KEY_LONGVARCHAR:
+      s += "LongVarChar";
+      break;
+
+   default:
+      throw pso::Exception( "KeyDefinitionODBC::GetNext",
+                            PSO_INVALID_FIELD_TYPE );
+   }
+
+   currentKey++;
+
+   return s;
 }
 
 // --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--
