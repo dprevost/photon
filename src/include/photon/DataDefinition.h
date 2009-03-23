55320c46b7d4366b5edf75803da07efd23c5f1f8
Daniel Prevost <dprevost@users.sourceforge.net>
2009-03-23 12:02:10 -0400

    Adding DataDefinition header file. Work in progress.

---
+++ src/include/photon/DataDefinition.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2009 Daniel Prevost <dprevost@photonsoftware.org>
+ *
+ * This file is part of Photon (photonsoftware.org).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 or version 3 as published by the 
+ * Free Software Foundation and appearing in the file COPYING.GPL2 and 
+ * COPYING.GPL3 included in the packaging of this software.
+ *
+ * Licensees holding a valid Photon Commercial license can use this file 
+ * in accordance with the terms of their license.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#ifndef PSO_DATA_DEFINITION_H
+#define PSO_DATA_DEFINITION_H
+
+#include <photon/psoCommon.h>
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \file
+ * This file provides the API needed to access a Photon data definition.
+ */
+/**
+ * \defgroup psoDataDefinition_c API functions for Photon data definitions.
+ * 
+ */
+/*@{*/
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * \brief Open an existing data definition.
+ *
+ * \param[in] definitionHandle The handle to the data definition (see ::psoFolderOpen).
+ *
+ * \return 0 on success or a ::psoErrors on error.
+ */
+PHOTON_EXPORT
+int psoDataDefOpen( PSO_HANDLE * definitionHandle );
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
