14d94e1ca9775b8de9383e6bd5134f9febe9e753
Daniel Prevost <dprevost@users.sourceforge.net>
2009-03-23 19:16:22 -0400

    Update to datadefinition header file - work in progress.

--- src/include/photon/DataDefinition.h
+++ src/include/photon/DataDefinition.h
@@ -42,13 +42,94 @@ extern "C" {
 /* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
 
 /**
- * \brief Open an existing data definition.
+ * \brief Closes the data definition.
  *
- * \param[in] definitionHandle The handle to the data definition (see ::psoFolderOpen).
+ * This function terminates the current access to the data definition.  
+ * The definition itself is left untouched in shared memory.
+ * \param[in] definitionHandle The handle to the data definition.
  *
  * \return 0 on success or a ::psoErrors on error.
  */
 PHOTON_EXPORT
-int psoDataDefOpen( PSO_HANDLE * definitionHandle );
+int psoDataDefClose( PSO_HANDLE definitionHandle );
 
 /* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * \brief Creates a new data definition.
+ *
+ * \param[in]  sessionHandle The handle to the current session.
+ * \param[in]  definitionName The name of the data definition. 
+ * \param[in]  nameLengthInBytes The length of \em definitionName (in bytes).
+ * \param[in]  dataDef The data definition (as an opaque type)
+ * \param[in]  dataDefLength The length in bytes of the buffer \em dataDef. 
+ * \param[out] definitionHandle The handle to the data definition.
+ *
+ * \return 0 on success or a ::psoErrors on error.
+ */
+PHOTON_EXPORT
+int psoDataDefCreate( PSO_HANDLE            sessionHandle,
+                      const char          * definitionName,
+                      psoUint32             nameLengthInBytes,
+                      const unsigned char * dataDef,
+                      psoUint32             dataDefLength,
+                      PSO_HANDLE          * definitionHandle );
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * \brief Retrieves a data definition.
+ * 
+ * You can call the function ::psoDataDefGetLength to retrieve the 
+ * length of the data definition. This allows you to allocate 
+ * the proper size for the \em dataDef buffer.
+ *
+ * \param[in]  definitionHandle The handle to the data definition.
+ * \param[out] dataDef The data definition (as an opaque type). You are
+ *             responsible for allocating the memory needed for this buffer. 
+ * \param[in]  dataDefLength The length in bytes of the buffer \em dataDef. 
+ *
+ * \return 0 on success or a ::psoErrors on error.
+ */
+PHOTON_EXPORT
+int psoDataDefGet( PSO_HANDLE      definitionHandle,
+                   unsigned char * dataDef,
+                   psoUint32       dataDefLength );
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * \brief Retrieves the length of the data definition.
+ * 
+ * This function is a helper function for ::psodataDefGet. 
+ * It allows you to allocate the proper size for the data definition.
+ *
+ * \param[in]  definitionHandle The handle to the data definition.
+ * \param[in]  dataDefLength The length in bytes of the data definition. 
+ *
+ * \return 0 on success or a ::psoErrors on error.
+ */
+PHOTON_EXPORT
+int psoDataDefGetLength( PSO_HANDLE   definitionHandle,
+                         psoUint32  * dataDefLength );
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/**
+ * \brief Open an existing data definition.
+ *
+ * \param[in]  sessionHandle The handle to the current session.
+ * \param[in]  definitionName The name of the data definition. 
+ * \param[in]  nameLengthInBytes The length of \em definitionName (in bytes).
+ * \param[out] definitionHandle The handle to the data definition.
+ *
+ * \return 0 on success or a ::psoErrors on error.
+ */
+PHOTON_EXPORT
+int psoDataDefOpen( PSO_HANDLE   sessionHandle,
+                    const char * definitionName,
+                    psoUint32    nameLengthInBytes,
+                    PSO_HANDLE * definitionHandle );
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
