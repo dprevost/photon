bba0c105fd05471f813ed15d194b14e779eae677
Daniel Prevost <dprevost@users.sourceforge.net>
2009-05-14 19:12:53 -0400

    Cleanup of api/queue tests.

---
+++ src/API/Tests/Queue/RecordDef.c
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2009 Daniel Prevost <dprevost@photonsoftware.org>
+ *
+ * This file is part of Photon (photonsoftware.org).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 or version 3 as published by the 
+ * Free Software Foundation and appearing in the file COPYING.GPL2 and 
+ * COPYING.GPL3 included in the packaging of this software.
+ *
+ * Licensees holding a valid Photon Commercial license can use this file 
+ * in accordance with the terms of their license.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/Common.h"
+#include <photon/photon.h>
+#include "Tests/PrintError.h"
+#include "API/Queue.h"
+
+const bool expectedToPass = true;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main( int argc, char * argv[] )
+{
+   PSO_HANDLE objHandle, sessionHandle;
+   int errcode;
+   psoObjectDefinition queueDef = { PSO_QUEUE, 0, 0, 0 };
+   psoFieldDefinition fields[1] = {
+      { "Field_1", PSO_VARCHAR, {10} }
+   };
+   PSO_HANDLE dataDefHandle, returnedDef;
+   
+   if ( argc > 1 ) {
+      errcode = psoInit( argv[1] );
+   }
+   else {
+      errcode = psoInit( "10701" );
+   }
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   errcode = psoInitSession( &sessionHandle );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoCreateFolder( sessionHandle,
+                              "/api_queue_record_def",
+                              strlen("/api_queue_record_def") );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoDataDefCreate( sessionHandle,
+                               "api_queue_record_def",
+                               strlen("api_queue_record_def"),
+                               PSO_DEF_PHOTON_ODBC_SIMPLE,
+                               (unsigned char *)fields,
+                               sizeof(psoFieldDefinition),
+                               &dataDefHandle );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoCreateQueue( sessionHandle,
+                             "/api_queue_record_def/test",
+                             strlen("/api_queue_record_def/test"),
+                             &queueDef,
+                             dataDefHandle );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoQueueOpen( sessionHandle,
+                           "/api_queue_record_def/test",
+                           strlen("/api_queue_record_def/test"),
+                           &objHandle );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   /* Invalid arguments to tested function. */
+
+   errcode = psoQueueRecordDefinition( NULL, &returnedDef );
+   if ( errcode != PSO_NULL_HANDLE ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoQueueRecordDefinition( sessionHandle, &returnedDef );
+   if ( errcode != PSO_WRONG_TYPE_HANDLE ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoQueueRecordDefinition( objHandle, NULL );
+   if ( errcode != PSO_NULL_POINTER ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   /* End of invalid args. This call should succeed. */
+   errcode = psoQueueRecordDefinition( objHandle, &returnedDef );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoQueueClose( objHandle );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+
+   errcode = psoDataDefClose( returnedDef );
+   if ( errcode != PSO_OK ) {
+      fprintf( stderr, "err: %d\n", errcode );
+      ERROR_EXIT( expectedToPass, NULL, ; );
+   }
+   
+   psoExit();
+
+   return 0;
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
