02ec02d6e7105164d0b6f72a3db6d92300845367
Daniel Prevost <dprevost@users.sourceforge.net>
2009-05-16 17:05:18 -0400

    Starting to fix the c++ tests. work in progress.

---
+++ src/C++/Tests/DataDefinition/Close.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2009 Daniel Prevost <dprevost@photonsoftware.org>
+ *
+ * This file is part of Photon (photonsoftware.org).
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 or version 3 as published by the 
+ * Free Software Foundation and appearing in the file COPYING.GPL2 and 
+ * COPYING.GPL3 included in the packaging of this software.
+ *
+ * Licensees holding a valid Photon Commercial license can use this file 
+ * in accordance with the terms of their license.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+#include "Common/Common.h"
+#include <photon/photon>
+#include "Tests/PrintError.h"
+#include <iostream>
+
+using namespace std;
+using namespace pso;
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+int main( int argc, char * argv[] )
+{
+   DataDefinition dataDef;
+   
+
+#if 0   
+   ObjDefinition def( 6, PSO_QUEUE );
+   string name;
+   
+   try {
+      def.AddField( "abc", 0, PSO_INTEGER, 4, 0, 0, 0, 0 );
+      // Should never come here
+      cerr << "Test failed - line " << __LINE__ << endl;
+      return 1;
+   }
+   catch( pso::Exception exc ) {
+      if ( exc.ErrorCode() != PSO_INVALID_FIELD_NAME ) {
+         cerr << "Test failed - line " << __LINE__ << ", error = " << exc.Message() << endl;
+         return 1;
+      }
+   }
+
+   try {
+      def.AddField( name, PSO_INTEGER, 4, 0, 0, 0, 0 );
+      // Should never come here
+      cerr << "Test failed - line " << __LINE__ << endl;
+      return 1;
+   }
+   catch( pso::Exception exc ) {
+      if ( exc.ErrorCode() != PSO_INVALID_FIELD_NAME ) {
+         cerr << "Test failed - line " << __LINE__ << ", error = " << exc.Message() << endl;
+         return 1;
+      }
+   }
+#endif
+}
