eb329efed57b855c4b96eea4a0dc2a8cc4ece0bb
Daniel Prevost <dprevost@users.sourceforge.net>
2009-05-26 09:40:41 -0400

    Working on arraylist serializer - in progress.

--- src/Java/jni_src/jni_arrayListSerializer.c
+++ src/Java/jni_src/jni_arrayListSerializer.c
@@ -26,47 +26,83 @@
 #include "org_photon_serializer_ArrayListSerializer.h"
 //#include "API/Queue.h"
 
-//jfieldID g_idQueueHandle;
+jfieldID g_idDataDef;
 
 /* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
 
 /*
- * Class:     org_photon_BaseQueue
+ * Class:     org_photon_serializer_ArrayListSerializer
  * Method:    initIDs
  * Signature: ()V
  */
-//JNIEXPORT void JNICALL
-//Java_org_photon_ArrayListSerializer_initIDs( JNIEnv * env, jclass queueClass )
-//{
-//   g_idQueueHandle = (*env)->GetFieldID( env, queueClass, "handle", "J" );
-//   if ( g_idQueueHandle == NULL ) return;
-//}
-
-/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
-
-/*
- * Class:     org_photon_ArrayListSerializer
- * Method:    psoPackObject
- * Signature: (Ljava/util/ArrayList;)[B
- */
-JNIEXPORT jbyteArray JNICALL
-Java_org_photon_ArrayListSerializer_psoPackObject( JNIEnv * env,
-                                                   jobject  jobj,
-                                                   jobject  jlist )
+JNIEXPORT void JNICALL
+Java_org_photon_serializer_ArrayListSerializer_initIDs( JNIEnv * env, 
+                                                        jclass   class)
 {
+   g_idDataDef = (*env)->GetFieldID( env, class, "dataDef", "Ljava/util/ArrayList;" );
+   if ( g_idDataDef == NULL ) return;
 }
 
 /* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
 
 /*
- * Class:     org_photon_ArrayListSerializer
+ * Class:     org_photon_serializer_ArrayListSerializer
+ * Method:    psoPackObject
+ * Signature: (Ljava/util/ArrayList;)[B
+ */
+JNIEXPORT jbyteArray JNICALL 
+Java_org_photon_serializer_ArrayListSerializer_psoPackObject( JNIEnv * env,
+                                                              jobject  jobj,
+                                                              jobject  jlist )
+{
+   jbyteArray jdataDef;	
+   psoFieldDefinition * dataDef;
+   jclass arrayClass, itemClass;
+   jmethodID methodSize, methodGet;
+   int size, i;
+   jobject jitem;
+   
+   jdataDef = (*env)->GetObjectField( env, jobj, g_idDataDef );
+   dataDef = (psoFieldDefinition *)
+             (*env)->GetByteArrayElements( env, jdataDef, NULL );
+   if ( dataDef == NULL ) return NULL;
+   
+   arrayClass = (*env)->FindClass( env, "java/util/ArrayList" );
+   if ( arrayClass == NULL ) return NULL;
+   
+   methodSize = (*env)->GetMethodID( env, arrayClass, "size", "()I" );
+   if ( methodSize == NULL ) return NULL;
+   
+   size = (*env)->CallIntMethod( env, jlist, methodSize );
+
+   methodGet = (*env)->GetMethodID( env, arrayClass, "get", "(I)Ljava.lang.Object;" );
+
+   for ( i = 0; i < size; ++i ) {
+      jitem = (*env)->CallObjectMethod( env, jlist, methodGet, i );
+
+      itemClass = (*env)->GetObjectClass( env, jitem );
+      
+      (*env)->isinstanceof( env, );
+      switch ( dataDef[i].type ) {
+
+      case PSO_INTEGER:
+      }
+      
+   }
+}
+
+/* --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-- */
+
+/*
+ * Class:     org_photon_serializer_ArrayListSerializer
  * Method:    psoUnpackObject
  * Signature: ([B)Ljava/util/ArrayList;
  */
 JNIEXPORT jobject JNICALL
-Java_org_photon_ArrayListSerializer_psoUnpackObject( JNIEnv   * env,
-                                                     jobject    jobj,
-                                                     jbyteArray jdata )
+Java_org_photon_serializer_ArrayListSerializer_psoUnpackObject( 
+   JNIEnv   * env,
+   jobject    jobj,
+   jbyteArray jArray )
 {
 }
 
